083b092581fb358352f28f46005c9c8dd40fb78e
NHutsuliak <NHutsuliak@VINW0260>
2021-07-07 17:47:45 +0300

    Add project files.

---
+++ Accounts/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+using AccountsLibrary;
+namespace Accounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var accountsData = new AccountData();
+            accountsData.getStringFromLinkToFile();
+
+
+            
+
+
+
+
+
+            foreach (var item in accountsData)
+            {
+                var str = String.Format("| {0,12} | {1,15} | {2,8} | {3,8} | {4,20} | {5,3} |",
+                    item.Name, item.Surname, item.Balance, item.Currency, item.RegistrationCity, item.Age);
+
+                Console.WriteLine("-------------------------------------------------------------------------------------");
+
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AccountsLibrary/AccountData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+namespace AccountsLibrary
+{
+    public interface IAccounts
+    {
+
+        public string DrawHeader()
+        {
+            return String.Format("| {0,12} | {1,15} | {2,8} | {3,8} | {4,20} | {5,3} |",
+                "Name", "Surname", "Balance", "Currency", "City", "Age"); ;
+
+        }
+
+    }
+
+    public class AccountData
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Balance { get; set; }
+        public string Currency { get; set; }
+        public string RegistrationCity { get; set; }
+        public int Age { get; set; }
+
+        public string getStringFromLinkToFile()
+        {
+            string fileName = Console.ReadLine();
+            return File.ReadAllText(fileName);
+
+        }
+
+        public string DrawLine()
+        {
+            return String.Format("| {0,12} | {1,15} | {2,8} | {3,8} | {4,20} | {5,3} |",
+                Name, Surname, Balance, Currency, RegistrationCity, Age);
+        }
+    }
+
+
+    public class AccountsFromJson : AccountData, IAccounts
+    {
+        public AccountsFromJson()
+        {
+            var accountsFromJson = JsonSerializer.Deserialize<List<AccountData>>(getStringFromLinkToFile());
+            DrawHeader();
+            DrawLine();
+        }
+        public string DrawHeader()
+        {
+            return String.Format("| {0,12} | {1,15} | {2,8} | {3,8} | {4,20} | {5,3} |",
+                "Name", "Surname", "Balance", "Currency", "City", "Age"); ;
+
+        }
+        
+
+    }
+}
