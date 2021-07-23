9e245b3aa82e4380b343887efab68bf11c4cfb9a
NHutsuliak <NHutsuliak@VINW0260>
2021-07-23 18:18:22 +0300

    v.7

--- AccountsLibraryTests/BalanceSumTest.cs
+++ AccountsLibraryTests/BalanceSumTest.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System.IO;
 using Xunit;
 using AccountsWithSum;
+using Accounts;
 
 namespace AccountsLibraryTests
 {
@@ -12,14 +13,43 @@ namespace AccountsLibraryTests
         [Fact]
         public void test1()
         {
-            // Arrange
+
+            //Arrange
+
+            var with = new List<AccountsModelWithSumBalance>()
+           {
+               new AccountsModelWithSumBalance()
+               {
+                   Name = "Name",
+                   Surname = "Surname",
+                   Balance = 51,
+                   Currency = "Curency",
+                   RegistrationCity = "city",
+                   Age = 10
+               }
+           };
+            var expected = with.Select(x => x.Balance);
+
+            var list = new List<AccountsModel>()
+           {
+               new AccountsModel()
+               {
+                   Name = "Name",
+                   Surname = "Surname",
+                   Balance = new List<decimal> { 1, 2, 3, 45 },
+                   Currency = "Curency",
+                   RegistrationCity = "city",
+                   Age = 10
+               }
+           };
 
             // Act
-            
 
-
+            var a = new AccountsModelWithSumBalance();
+            var a2 = a.GetAccountsWithSumBalances(list);
+            var actual = a2.Select(x => x.Balance);
             // Assert
-
+            Assert.Equal(expected, actual);
         }
     }
 }
--- AccountsWithSum/AccountsModelWithSumBalance.cs
+++ AccountsWithSum/AccountsModelWithSumBalance.cs
@@ -28,6 +28,9 @@ namespace AccountsWithSum
            
             return accountsModelWith ;
         }
+
+
     }
+
     
 }
